2ce2d44c27c157f298e96f2554a807f42d687740
PonyJackal <pony1125@outlook.com>
2023-02-19 18:18:30 -0800

    feat: add deposit nfts

---
+++ src/test/SeacowsPairERC721ERC20/WhenDepositNFTs.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.4;
+
+import "forge-std/Test.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ICurve } from "../../bondingcurve/ICurve.sol";
+import { SeacowsPairERC20 } from "../../SeacowsPairERC20.sol";
+import { SeacowsPairFactory } from "../../SeacowsPairFactory.sol";
+import { SeacowsPair } from "../../SeacowsPair.sol";
+import { TestWETH } from "../../TestCollectionToken/TestWETH.sol";
+import { TestERC20 } from "../../TestCollectionToken/TestERC20.sol";
+import { TestERC721 } from "../../TestCollectionToken/TestERC721.sol";
+import { TestERC721Enumerable } from "../../TestCollectionToken/TestERC721Enumerable.sol";
+import { WhenCreatePair } from "../base/WhenCreatePair.t.sol";
+
+/// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
+/// https://book.getfoundry.sh/forge/writing-tests
+contract WhenDepositNFTs is WhenCreatePair {
+    SeacowsPairERC20 internal erc721ERC20Pair;
+    SeacowsPairERC20 internal erc721EnumerableERC20Pair;
+
+    TestERC721Enumerable internal nftEnumerable;
+    TestERC721 internal nft;
+    TestERC20 internal token;
+
+    function setUp() public virtual override(WhenCreatePair) {
+        WhenCreatePair.setUp();
+
+        token = new TestERC20();
+        token.mint(owner, 1000 ether);
+
+        nftEnumerable = new TestERC721Enumerable();
+        nftEnumerable.safeMint(owner);
+
+        nft = new TestERC721();
+        nft.safeMint(owner);
+
+        /** Approve Bonding Curve */
+        seacowsPairFactory.setBondingCurveAllowed(linearCurve, true);
+
+        /** Create ERC721Enumerable-ERC20 NFT Pair */
+        vm.startPrank(owner);
+        uint256[] memory nftEnumerableIds = new uint256[](1);
+        nftEnumerableIds[0] = 0;
+        token.approve(address(seacowsPairFactory), 1 ether);
+        nftEnumerable.setApprovalForAll(address(seacowsPairFactory), true);
+        erc721EnumerableERC20Pair = createNFTPair(token, nftEnumerable, linearCurve, payable(owner), 2.2 ether, 2 ether, nftEnumerableIds, 1 ether);
+
+        /** Create ERC721-ERC20 NFT Pair */
+        uint256[] memory nftIds = new uint256[](1);
+        nftIds[0] = 0;
+        token.approve(address(seacowsPairFactory), 1 ether);
+        nft.setApprovalForAll(address(seacowsPairFactory), true);
+        erc721ERC20Pair = createNFTPair(token, nft, linearCurve, payable(owner), 2.2 ether, 2 ether, nftIds, 1 ether);
+        vm.stopPrank();
+    }
+}
