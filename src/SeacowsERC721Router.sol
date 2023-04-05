d72cb42f47a74dcae1d27673fc642b39f90c1fe0
PonyJackal <pony1125@outlook.com>
2023-04-05 07:39:22 -0700

    feat: add swapNFTsForToken

--- src/SeacowsERC721Router.sol
+++ src/SeacowsERC721Router.sol
@@ -2,9 +2,17 @@
 pragma solidity ^0.8.0;
 
 import { SeacowsPairERC721 } from "./pairs/SeacowsPairERC721.sol";
+import { ISeacowsPairERC721 } from "./interfaces/ISeacowsPairERC721.sol";
 
 contract SeacowsERC721Router {
-    function swapNFTsForToken(SeacowsPairERC721 _pair, uint256[] calldata _nftIds, uint256 _minOutput, address payable _recipient)
+    /**
+        @notice Swap NFTs for Token
+        @param _pair ERC721 pair
+        @param _nftIds ERC721 nft ids
+        @param _minOutput The minimum expected erc20 token amount
+        @param _recipient Token recipient address
+     */
+    function swapNFTsForToken(ISeacowsPairERC721 _pair, uint256[] calldata _nftIds, uint256 _minOutput, address payable _recipient)
         external
         returns (uint256 outputAmount)
     {
